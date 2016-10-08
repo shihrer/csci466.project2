9d880e8686d3e9b90ef2a424d2581671a853b788
Michael Shihrer <michael.shihrer@gmail.com>
2016-10-08 14:57:12 -0600

    Works if there's no corruption.

--- Network.py
+++ Network.py
@@ -10,7 +10,7 @@ import random
 class NetworkLayer:
     #configuration parameters
     prob_pkt_loss = 0
-    prob_byte_corr = .5
+    prob_byte_corr = 0
     prob_pkt_reorder = 0
     
     #class variables
