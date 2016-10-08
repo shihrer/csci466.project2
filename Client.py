814a97e332daa17b73a7cd421991b24d14ca5b8a
Michael Shihrer <michael.shihrer@gmail.com>
2016-10-07 20:41:09 -0600

    Modified client/server to 2.1

--- Client.py
+++ Client.py
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     rdt = RDT.RDT('client', args.server, args.port)
     for msg_S in msg_L:
         print('Converting: '+msg_S)
-        rdt.rdt_1_0_send(msg_S)
+        rdt.rdt_2_1_send(msg_S)
        
         # try to receive message before timeout 
         msg_S = None
         while msg_S == None:
-            msg_S = rdt.rdt_1_0_receive()
+            msg_S = rdt.rdt_2_1_receive()
             if msg_S is None:
                 if time_of_last_data + timeout < time.time():
                     break
